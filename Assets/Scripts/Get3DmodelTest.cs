7e38ecab245640625ab54963fcd05049465d2e74
keefejulian24 <keefejulian24@gmail.com>
2017-02-09 22:57:40 +0800

    Load 3D Model, See from scene view, can't from game view

---
+++ Assets/Scripts/Get3DmodelTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace URECA
+{
+    public class Get3DmodelTest : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            // Instantiates a prefab named "enemy" located in any Resources
+            // folder in your project's Assets folder.
+            GameObject model = Instantiate(Resources.Load("petronas", typeof(GameObject))) as GameObject;
+            model.transform.SetParent(GetComponent<Canvas>().transform, false);
+            model.transform.SetAsFirstSibling();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
