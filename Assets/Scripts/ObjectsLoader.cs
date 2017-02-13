a25828d952cdcfa22b74b18c2f6009a8bcbd6e38
albertlucianto <albertlucianto@gmail.com>
2017-02-13 20:24:45 +0800

    Added button to load files specified by InputField

--- Assets/Scripts/ObjectsLoader.cs
+++ Assets/Scripts/ObjectsLoader.cs
@@ -7,18 +7,23 @@ namespace URECA
 {
 	public class ObjectsLoader : MonoBehaviour {
 
-		private List<ObjectXML> allObjects;
+		private static List<ObjectXML> allObjects;
+		private static InputField inputFileName;
 		//private List<GameObject> unityObjects;
 
 		// Use this for initialization
-		void Start () {
-			
+		public static void Load() {
+
+			inputFileName = GameObject.FindWithTag ("InputFileName").GetComponent<InputField> ();
+			string fileName = inputFileName.text;
+
 			GameObject mainCamera = GameObject.FindWithTag ("MainCamera");
 			mainCamera.transform.position = GameObject.FindWithTag ("Canvas").transform.position;
 
 			mainCamera.transform.position += new Vector3 (0.0f, 0.0f, -432.0f); //to adjust visible area with canvas
 
-			XMLDecoder.loadData("Assets/Save_files/Data.xml");
+			XMLDecoder.clearData ();
+			XMLDecoder.loadData("Assets/Save_files/" + fileName + ".xml");
 			allObjects = XMLDecoder.getData();
 
 			foreach (var objectXML in allObjects) {
@@ -32,6 +37,9 @@ namespace URECA
 				x.transform.SetParent(GameObject.FindWithTag ("Canvas").transform, false);
 
 			}
+		}
+
+		public void Start(){
 		}
 	}
 }
---
+++ Assets/Scripts/OpenFileButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace URECA
+{
+	public class OpenFileButton : MonoBehaviour {
+
+		private Button openButton;
+
+		// Use this for initialization
+		void Start () {
+			openButton = GetComponent<Button> ();
+
+			openButton.onClick.AddListener(ObjectsLoader.Load);
+		}
+	
+	}
+}
